a0d73f7444b68ee386d28a745f39367950d31391
puraminy <pouramini@gmail.com>
2022-10-31 20:15:52 +0330

    commit

--- Deep/run.sh
+++ Deep/run.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-CUDA_VISIBLE_DEVICES=0,1 \
-TOKENIZERS_PARALLELISM=false \
+#CUDA_VISIBLE_DEVICES=0,1 \
+TOKENIZERS_PARALLELISM=true \
 HF_DATASETS_CACHE=~/.cache/huggingface/datasets \
 python run.py
